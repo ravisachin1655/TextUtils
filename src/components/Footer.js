15f7e72f35148e06eb6d1c3a15c7906528327f06
sachin <ravisachin295@gmail.com>
2021-09-01 12:22:14 +0530

    sauhueh

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <>
+<footer className="page-footer font-small blue mt-auto">
+
+  <div className="footer-copyright text-center py-3">© 2021 Copyright:
+    <a href="https://oxyboy.netlify.app">  Sachin Ravi</a>
+  </div>
+
+</footer>
+
+        </>
+    )
+}
+
+export default footer;
